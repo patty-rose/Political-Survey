dda5e2c03876cc788437e5340e8046006db46a86
patty-rose <oteropatty@gmail.com>
2022-04-06 19:30:11 -0700

    add basic css styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-jQuery(document).ready(function() {
-  
+$(document).ready(function() {
+  $("form#pol").submit(function(event) {
+    event.preventDefault();
+
+    const cars = $("#cars").val();
+    const wake = $("#wake").val();
+    const coffee = $("#coffee").val();
+    
+    if (cars === "4" && wake === "4" && coffee === "4") {
+      $("#liberal").show();
+    } else if (cars === "3" && wake == "3" && coffee === "3") {
+      $("#moderate").show();
+    } else {
+      $("#conservative").show();
+    }
+  });
 })
